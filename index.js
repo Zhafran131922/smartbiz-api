701fe00f561e541b67556c7824e2252ed8ee92c3
Zhafran131922 <138988801+Zhafran131922@users.noreply.github.com>
2023-12-19 10:36:30 +0700

    Update index.js
    mysql2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Hapi = require('@hapi/hapi');
 const Joi = require('@hapi/joi');
-const mysql = require('mysql12');
+const mysql = require('mysql2');
 const nodemailer = require('nodemailer');
 const { generateSixDigitToken } = require('./utils');
 
